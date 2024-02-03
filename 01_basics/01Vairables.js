a72a3920dd759cfb09d62493d3bc4498333133ee
vanshnenwani16 <119725973+vanshnenwani16@users.noreply.github.com>
2024-02-03 05:02:20 +0000

    container congig done

---
+++ 01_basics/01Vairables.js
@@ -0,0 +1,6 @@
+const accountId =1245;
+let accountEmail = "vansh@"
+var accountPassword ="1452"
+accountCity="Hingoli"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
